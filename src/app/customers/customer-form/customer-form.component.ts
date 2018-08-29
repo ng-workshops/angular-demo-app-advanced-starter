e3a75df4a20fe8894a7c93d5bb35ff86a92e2457
Andreas Krummsdorf <andreas.krummsdorf@doksafe.de>
2018-08-29 13:27:50 +0200

    optimize

--- src/app/customers/customer-form/customer-form.component.ts
+++ src/app/customers/customer-form/customer-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Customer } from '../customer.model';
-import { switchMap, filter } from 'rxjs/operators';
+import { switchMap, filter, map } from 'rxjs/operators';
 import { CustomerService } from '../customer.service';
 import { MatSnackBar } from '@angular/material';
 
@@ -19,15 +19,16 @@ export class CustomerFormComponent implements OnInit {
     private router: Router,
     private customerService: CustomerService,
     private snackBar: MatSnackBar
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.form = Customer.toFormGroup();
 
     this.route.paramMap
       .pipe(
-        filter(params => params.get('id') !== 'new'),
-        switchMap(params => this.customerService.getById(params.get('id')))
+        map(params => params.get('id')),
+        filter(id => id !== 'new'),
+        switchMap(id => this.customerService.getById(id))
       )
       .subscribe(customer => {
         this.form.patchValue(customer);
@@ -37,14 +38,15 @@ export class CustomerFormComponent implements OnInit {
   submit() {
     const data = this.form.getRawValue();
     const save$ = data.id
-      ? this.customerService.update.bind(this.customerService) :
-      this.customerService.create.bind(this.customerService);
+      ? this.customerService.update.bind(this.customerService)
+      : this.customerService.create.bind(this.customerService);
 
-    save$(data)
-      .subscribe(_ => {
-        this.snackBar.open(`Customer ${data.name} saved successfully.`, '', { duration: 2000 });
-        this.cancel();
+    save$(data).subscribe(_ => {
+      this.snackBar.open(`Customer ${data.name} saved successfully.`, '', {
+        duration: 2000
       });
+      this.cancel();
+    });
   }
 
   cancel() {
