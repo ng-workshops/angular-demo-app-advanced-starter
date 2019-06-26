5be7083963c42947cf02968fcbbf641cd79c23af
Andreas Krummsdorf <andreas.krummsdorf@doksafe.de>
2019-06-26 12:20:27 +0200

    Update router store

--- src/app/core/router/router.effects.ts
+++ src/app/core/router/router.effects.ts
@@ -1,11 +1,11 @@
 import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { map, tap } from 'rxjs/operators';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
 import * as RouterActions from './router.actions';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class RouterEffects {
   constructor(
     private actions$: Actions,
@@ -13,24 +13,32 @@ export class RouterEffects {
     private location: Location
   ) {}
 
-  @Effect({ dispatch: false })
-  navigate$ = this.actions$.pipe(
-    ofType(RouterActions.GO),
-    map((action: RouterActions.Go) => action.payload),
-    tap(({ path, query: queryParams, extras }) => {
-      this.router.navigate(path, { queryParams, ...extras });
-    })
+  navigate$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(RouterActions.navigate),
+        tap(({ path, query: queryParams, extras }) => {
+          this.router.navigate(path, { queryParams, ...extras });
+        })
+      ),
+    { dispatch: false }
   );
 
-  @Effect({ dispatch: false })
-  navigateBack$ = this.actions$.pipe(
-    ofType(RouterActions.BACK),
-    tap(() => this.location.back())
+  back$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(RouterActions.back),
+        tap(() => this.location.back())
+      ),
+    { dispatch: false }
   );
 
-  @Effect({ dispatch: false })
-  navigateForward$ = this.actions$.pipe(
-    ofType(RouterActions.FORWARD),
-    tap(() => this.location.forward())
+  forward$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(RouterActions.forward),
+        tap(() => this.location.forward())
+      ),
+    { dispatch: false }
   );
 }
--- src/app/core/router/router.serializer.ts
+++ src/app/core/router/router.serializer.ts
@@ -1,28 +1,28 @@
-import {
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Params
-} from '@angular/router';
-import * as fromRouter from '@ngrx/router-store';
+import { Params, RouterStateSnapshot } from '@angular/router';
+import { RouterStateSerializer } from '@ngrx/router-store';
 
 export interface RouterStateUrl {
   url: string;
-  queryParams: Params;
   params: Params;
+  queryParams: Params;
 }
 
-export class CustomSerializer
-  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
   serialize(routerState: RouterStateSnapshot): RouterStateUrl {
-    const { url } = routerState;
-    const { queryParams } = routerState.root;
+    let route = routerState.root;
 
-    let state: ActivatedRouteSnapshot = routerState.root;
-    while (state.firstChild) {
-      state = state.firstChild;
+    while (route.firstChild) {
+      route = route.firstChild;
     }
-    const { params } = state;
 
-    return { url, queryParams, params };
+    const {
+      url,
+      root: { queryParams }
+    } = routerState;
+    const { params } = route;
+
+    // Only return an object including the URL, params and query params
+    // instead of the entire snapshot
+    return { url, params, queryParams };
   }
 }
